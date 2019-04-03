727709998bb6bbdbc804d9fcefb6f1ebb702f11d
Philippe Marguinaud <pmarguinaud@hotmail.com>
2019-04-03 21:13:59 +0200

    Use alpha channel (same for all vertex in ColorFragmentShader.fragmentshader)

--- tutorial04bis.cpp
+++ tutorial04bis.cpp
@@ -61,7 +61,9 @@ int main (int argc, char * argv[])
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   
   // Enable depth test
-  glEnable(GL_DEPTH_TEST);
+  glEnable (GL_DEPTH_TEST);
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glCullFace (GL_BACK);
   glFrontFace (GL_CCW);
   glEnable (GL_CULL_FACE);

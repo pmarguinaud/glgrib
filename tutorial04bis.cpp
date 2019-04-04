9f823506e72990663e22de6728e6cea95219fc5a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2019-04-04 11:52:09 +0200

    Add keyboard control

--- tutorial04bis.cpp
+++ tutorial04bis.cpp
@@ -1,22 +1,64 @@
-// Include standard headers
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
-
-// Include GLEW
 #include <GL/glew.h>
-
-// Include GLFW
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
-
-// Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 #include "shader.hpp"
 
 extern "C" void load (const char *, int *, float **, float **, unsigned int *, unsigned int **, int);
+
+static float rc = 2.5, latc = 0., lonc = 0., fov = 50.;
+static bool do_rotate = false;
+
+static void key_callback (GLFWwindow * window, int key, int scancode, int action, int mods)
+{
+  if (action == GLFW_PRESS)
+    {
+      switch (key)
+        {
+          case GLFW_KEY_TAB:
+            do_rotate = ! do_rotate;
+            break;
+          case GLFW_KEY_W:
+            fov += 1.;
+            break;
+          case GLFW_KEY_Q:
+            fov -= 1.;
+            break;
+          case GLFW_KEY_6:
+            rc += 0.1;
+            break;
+          case GLFW_KEY_EQUAL:
+            rc -= 0.1;
+            break;
+          case GLFW_KEY_SPACE:
+            latc = 0.;
+            lonc = 0.;
+	    rc = 2.5;
+	    break;
+          case GLFW_KEY_UP:
+            latc = latc + 5.;
+            break;
+          case GLFW_KEY_DOWN:
+            latc = latc - 5.;
+            break;
+          case GLFW_KEY_LEFT:
+            lonc = lonc - 5.;
+            break;
+          case GLFW_KEY_RIGHT:
+            lonc = lonc + 5.;
+            break;
+	  default:
+	    break;
+	}
+    }
+}
+
 
 int main (int argc, char * argv[])
 {
@@ -36,7 +78,8 @@ int main (int argc, char * argv[])
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
-  window = glfwCreateWindow (1024, 1024, file, NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow (1024, 1024, file, NULL, NULL);
+
   if( window == NULL ){
   	fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
   	getchar();
@@ -81,11 +124,6 @@ int main (int argc, char * argv[])
   
   GLuint MatrixID = glGetUniformLocation(programID, "MVP");
   
-  glm::mat4 Projection = glm::perspective(glm::radians(50.0f), 1.0f / 1.0f, 0.1f, 100.0f);
-  glm::mat4 View       = glm::lookAt(glm::vec3 (2.5,0,0), glm::vec3 (0,0,0), glm::vec3 (0,0,1));
-  glm::mat4 Model      = glm::mat4(1.0f);
-  glm::mat4 MVP        = Projection * View * Model; 
-  
   int np;
   unsigned int nt;
   unsigned int * ind;
@@ -113,34 +151,51 @@ int main (int argc, char * argv[])
   free (ind);
   free (xyz);
   free (col);
+
+  glfwSetKeyCallback (window, key_callback);
   
-  do{
-  	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  while (1)
+    {
+      if (do_rotate)
+        lonc += 1.;
+      float xc = rc * glm::cos (glm::radians (lonc)) * glm::cos (glm::radians (latc)), 
+	    yc = rc * glm::sin (glm::radians (lonc)) * glm::cos (glm::radians (latc)),
+	    zc = rc *                                  glm::sin (glm::radians (latc));
+      glm::mat4 Projection = glm::perspective (glm::radians (fov), 1.0f / 1.0f, 0.1f, 100.0f);
+      glm::mat4 View       = glm::lookAt (glm::vec3 (xc,yc,zc), 
+		                          glm::vec3 (0,0,0), 
+					  glm::vec3 (0,0,1));
+      glm::mat4 Model      = glm::mat4 (1.0f);
+      glm::mat4 MVP        = Projection * View * Model; 
   
-  	glUseProgram(programID);
+      glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      
+      glUseProgram (programID);
+      
+      glUniformMatrix4fv (MatrixID, 1, GL_FALSE, &MVP[0][0]);
+      
+      glEnableVertexAttribArray (0);
+      glBindBuffer (GL_ARRAY_BUFFER, vertexbuffer);
+      glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+      
+      glEnableVertexAttribArray (1);
+      glBindBuffer (GL_ARRAY_BUFFER, colorbuffer);
+      glVertexAttribPointer (1, ncol, GL_FLOAT, GL_TRUE, ncol * sizeof (float), NULL);
+      
+      glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+      glDrawElements (GL_TRIANGLES, 3 * nt, GL_UNSIGNED_INT, NULL);
+      
+      glDisableVertexAttribArray (0);
+      glDisableVertexAttribArray (1);
+      
+      glfwSwapBuffers (window);
+      glfwPollEvents ();
   
-  	glUniformMatrix4fv (MatrixID, 1, GL_FALSE, &MVP[0][0]);
-  
-  	glEnableVertexAttribArray (0);
-  	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-  
-  	glEnableVertexAttribArray (1);
-  	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-  	glVertexAttribPointer (1, ncol, GL_FLOAT, GL_TRUE, ncol * sizeof (float), NULL);
-  
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-  	glDrawElements (GL_TRIANGLES, 3 * nt, GL_UNSIGNED_INT, NULL);
-  
-        glDisableVertexAttribArray (0);
-        glDisableVertexAttribArray (1);
-  
-  	glfwSwapBuffers (window);
-  	glfwPollEvents();
-  
-  } 
-  while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-  	   glfwWindowShouldClose(window) == 0 );
+      if (glfwGetKey (window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        break;
+      if (glfwWindowShouldClose (window) != 0) 
+        break;
+    } 
   
   glDeleteBuffers(1, &vertexbuffer);
   glDeleteBuffers(1, &colorbuffer);

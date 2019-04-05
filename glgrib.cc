0e87e35e83d3777bd5e46d9fd5a02957b756a362
Philippe Marguinaud <pmarguinaud@hotmail.com>
2019-04-05 20:18:26 +0200

    Make it modular

--- glgrib.cc
+++ glgrib.cc
@@ -20,9 +20,14 @@ typedef struct view_t
   GLuint MatrixID;
 } view_t;
 
+typedef struct prog_t
+{
+  GLuint programID;
+} prog_t;
+
 typedef struct obj_t
 {
-  GLuint programID, VertexArrayID;
+  GLuint VertexArrayID;
   GLuint vertexbuffer, colorbuffer, elementbuffer;
   int use_alpha = 1;
   unsigned int ncol, nt;
@@ -78,7 +83,7 @@ static void key_callback (GLFWwindow * window, int key, int scancode, int action
 
 
 static
-void display (const obj_t * obj, const view_t * view)
+void display (const prog_t * prog, const obj_t * obj, const view_t * view)
 {
   float xc = view->rc * glm::cos (glm::radians (view->lonc)) * glm::cos (glm::radians (view->latc)), 
         yc = view->rc * glm::sin (glm::radians (view->lonc)) * glm::cos (glm::radians (view->latc)),
@@ -91,9 +96,9 @@ void display (const obj_t * obj, const view_t * view)
   
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
-  glUseProgram (obj->programID);
-  
+  glUseProgram (prog->programID);
   glUniformMatrix4fv (view->MatrixID, 1, GL_FALSE, &MVP[0][0]);
+  
   
   glEnableVertexAttribArray (0);
   glBindBuffer (GL_ARRAY_BUFFER, obj->vertexbuffer);
@@ -109,18 +114,93 @@ void display (const obj_t * obj, const view_t * view)
   glDisableVertexAttribArray (0);
   glDisableVertexAttribArray (1);
 }
-      
 
-int main (int argc, char * argv[])
+static
+void obj_init (obj_t * obj, const char * file)
 {
-  const char * file = argv[1];
-  obj_t Obj;
+  glGenVertexArrays (1, &obj->VertexArrayID);
+  glBindVertexArray (obj->VertexArrayID);
   
+  unsigned int * ind;
+  float * xyz, * col;
+  obj->ncol = obj->use_alpha ? 4 : 3;
+
+  load (file, &obj->np, &xyz, &col, &obj->nt, &ind, obj->use_alpha);
+  
+  glGenBuffers (1, &obj->vertexbuffer);
+  glBindBuffer (GL_ARRAY_BUFFER, obj->vertexbuffer);
+  glBufferData (GL_ARRAY_BUFFER, 3 * obj->np * sizeof (float), xyz, GL_STATIC_DRAW);
+  
+
+  glGenBuffers (1, &obj->colorbuffer);
+  glBindBuffer (GL_ARRAY_BUFFER, obj->colorbuffer);
+  glBufferData (GL_ARRAY_BUFFER, obj->ncol * obj->np * sizeof (float), col, GL_STATIC_DRAW);
+  
+  glGenBuffers (1, &obj->elementbuffer);
+  glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, obj->elementbuffer);
+  glBufferData (GL_ELEMENT_ARRAY_BUFFER, 3 * obj->nt * sizeof (unsigned int), 
+		ind , GL_STATIC_DRAW);
+
+  free (ind);
+  free (xyz);
+  free (col);
+}
+
+static
+void view_init (prog_t * prog, view_t * view)
+{
+  view->MatrixID = glGetUniformLocation (prog->programID, "MVP");
+}
+
+static
+void view_free (view_t * view)
+{
+
+}
+      
+static
+void obj_free (obj_t * obj)
+{
+  glDeleteBuffers (1, &obj->vertexbuffer);
+  glDeleteBuffers (1, &obj->colorbuffer);
+  glDeleteVertexArrays (1, &obj->VertexArrayID);
+}
+
+static
+void prog_free (prog_t * prog)
+{
+  glDeleteProgram (prog->programID);
+}
+
+static
+void prog_init (prog_t * prog)
+{
+  prog->programID = LoadShaders ("TransformVertexShader.vertexshader", 
+		                 "ColorFragmentShader.fragmentshader");
+}
+
+static
+void gl_init ()
+{
+  glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+  glEnable (GL_DEPTH_TEST);
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glCullFace (GL_BACK);
+  glFrontFace (GL_CCW);
+  glEnable (GL_CULL_FACE);
+  glDepthFunc (GL_LESS); 
+}
+  
+GLFWwindow * new_glfw_window (const char * file)
+{
+  GLFWwindow * window;
+
   if (! glfwInit ())
     {
       fprintf (stderr, "Failed to initialize GLFW\n");
       getchar ();
-      return -1;
+      return NULL;
     }
   
   glfwWindowHint (GLFW_SAMPLES, 4);
@@ -129,14 +209,14 @@ int main (int argc, char * argv[])
   glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
   glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
-  GLFWwindow* window = glfwCreateWindow (1024, 1024, file, NULL, NULL);
+  window = glfwCreateWindow (1024, 1024, file, NULL, NULL);
 
   if (window == NULL)
     {
       fprintf (stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
       getchar ();
       glfwTerminate ();
-      return -1;
+      return NULL;
     }
 
   glfwMakeContextCurrent (window);
@@ -147,79 +227,54 @@ int main (int argc, char * argv[])
       fprintf (stderr, "Failed to initialize GLEW\n");
       getchar ();
       glfwTerminate ();
-      return -1;
+      return NULL;
     }
   
   glfwSetInputMode (window, GLFW_STICKY_KEYS, GL_TRUE);
-  
-  glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-  
-  glEnable (GL_DEPTH_TEST);
-  glEnable (GL_BLEND);
-  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glCullFace (GL_BACK);
-  glFrontFace (GL_CCW);
-  glEnable (GL_CULL_FACE);
-  
-  
-  glDepthFunc (GL_LESS); 
-  
-  glGenVertexArrays (1, &Obj.VertexArrayID);
-  glBindVertexArray (Obj.VertexArrayID);
-  
-  Obj.programID = LoadShaders ("TransformVertexShader.vertexshader", 
-		               "ColorFragmentShader.fragmentshader");
-  
-  View.MatrixID = glGetUniformLocation (Obj.programID, "MVP");
-  
-  unsigned int * ind;
-  float * xyz, * col;
-  Obj.ncol = Obj.use_alpha ? 4 : 3;
-
-  load (file, &Obj.np, &xyz, &col, &Obj.nt, &ind, Obj.use_alpha);
-  
-  glGenBuffers (1, &Obj.vertexbuffer);
-  glBindBuffer (GL_ARRAY_BUFFER, Obj.vertexbuffer);
-  glBufferData (GL_ARRAY_BUFFER, 3 * Obj.np * sizeof (float), xyz, GL_STATIC_DRAW);
-  
-
-  glGenBuffers (1, &Obj.colorbuffer);
-  glBindBuffer (GL_ARRAY_BUFFER, Obj.colorbuffer);
-  glBufferData (GL_ARRAY_BUFFER, Obj.ncol * Obj.np * sizeof (float), col, GL_STATIC_DRAW);
-  
-  glGenBuffers (1, &Obj.elementbuffer);
-  glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, Obj.elementbuffer);
-  glBufferData (GL_ELEMENT_ARRAY_BUFFER, 3 * Obj.nt * sizeof (unsigned int), 
-		ind , GL_STATIC_DRAW);
-
-  free (ind);
-  free (xyz);
-  free (col);
-
   glfwSetKeyCallback (window, key_callback);
+
+  return window;
+}
+  
+static 
+void free_glfw_window (GLFWwindow * window)
+{
+  glfwTerminate ();
+}
+
+int main (int argc, char * argv[])
+{
+  const char * file = argv[1];
+  obj_t Obj;
+  prog_t Prog;
+  GLFWwindow * Window = new_glfw_window (file);
+  
+  gl_init ();
+  prog_init (&Prog);
+  obj_init (&Obj, file);
+  view_init (&Prog, &View);
   
   while (1)
     {
       if (do_rotate)
         View.lonc += 1.;
 
-      display (&Obj, &View);
+      display (&Prog, &Obj, &View);
 
-      glfwSwapBuffers (window);
+      glfwSwapBuffers (Window);
       glfwPollEvents ();
   
-      if (glfwGetKey (window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      if (glfwGetKey (Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         break;
-      if (glfwWindowShouldClose (window) != 0) 
+      if (glfwWindowShouldClose (Window) != 0) 
         break;
     } 
   
-  glDeleteBuffers (1, &Obj.vertexbuffer);
-  glDeleteBuffers (1, &Obj.colorbuffer);
-  glDeleteProgram (Obj.programID);
-  glDeleteVertexArrays (1, &Obj.VertexArrayID);
+  view_free (&View);
+  obj_free (&Obj);
+  prog_free (&Prog);
   
-  glfwTerminate ();
+  free_glfw_window (Window);
   
   return 0;
 }

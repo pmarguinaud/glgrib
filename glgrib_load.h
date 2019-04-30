b2d710af2d49ae4716896df22b4b4f88b198a34d
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2019-04-30 10:22:39 +0200

    Cleaning

--- glgrib_load.h
+++ glgrib_load.h
@@ -1,8 +1,6 @@
 #ifndef _GLGRIB_LOAD_H
 #define _GLGRIB_LOAD_H
 
-void glgrib_load_z (const char *, int *, float **, unsigned int *, unsigned int **, float);
-void glgrib_load_rgb (const char *, unsigned char **, int);
 void glgrib_load (const char *, float **, int );
 
 #endif

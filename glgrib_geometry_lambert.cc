8f500b09afecce3900c729f7f917ca55b1faf2ab
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2019-05-27 13:21:44 +0200

    Embed projection definition in glgrib_geometry_lambert class

--- glgrib_geometry_lambert.cc
+++ glgrib_geometry_lambert.cc
@@ -10,103 +10,9 @@
 #include <iostream>
 #include <stdexcept>
 
-static const double rad2deg = 180.0 / M_PI;
-static const double deg2rad = M_PI / 180.0;
-static const double a = 6371229.0;
-
-class latlon_t
-{
-public:
-  latlon_t (double _lon, double _lat) : lon (_lon), lat (_lat) {}
-  double lat, lon;
-};
-
-class xy_t 
-{
-public:
-  xy_t (double _x, double _y) : x (_x), y (_y) {}
-  double x, y;
-  xy_t operator+ (const xy_t & p)
-  {
-    return xy_t (x + p.x, y + p.y);
-  }
-  xy_t operator- (const xy_t & p)
-  {
-    return xy_t (x - p.x, y - p.y);
-  }
-};
-
-class rtheta_t
-{
-public:
-  rtheta_t (double _r, double _theta) : r (_r), theta (_theta) {}
-  double r, theta;
-};
-
-static inline double sign (double a, double b)
-{
-  return b > 0 ? +fabs (a) : -fabs (a);
-}
-
-static inline double dist_2ref (latlon_t pt_coord, latlon_t ref_coord)
-{
-  double z = pt_coord.lon - ref_coord.lon;
-  z = z - sign (M_PI, z) * (1.0 + sign (1.0, fabs (z) - M_PI));
-  return -z * sign (1.0, z - M_PI);
-}
-
-class proj_t
-{
-public:
-  proj_t (double lon, double lat, double _pole) : ref_pt (lon, lat), pole (_pole)
-  {
-    kl = pole * sin (ref_pt.lat);
-    r_equateur = a * pow (cos (ref_pt.lat), 1.0 - kl) * pow (1.0 + kl, kl) / kl;
-  }
-  latlon_t ref_pt;
-  double pole;
-  double r_equateur;
-  double kl;
-
-  latlon_t rtheta_to_latlon (const rtheta_t & pt_rtheta)
-  {
-    return latlon_t (ref_pt.lon + pt_rtheta.theta / kl,
-                     pole * ((M_PI / 2.0) - 2.0 * atan(pow (pt_rtheta.r / r_equateur, 1.0 / kl))));
-  }
-  rtheta_t xy_to_rtheta (const xy_t & pt_xy)
-  {
-    double r = sqrt(pt_xy.x * pt_xy.x + pt_xy.y * pt_xy.y);
-    double tatng;
-    double theta;
-  
-    if (pt_xy.y == 0.0) 
-      tatng = (pt_xy.x == 0.0) ? M_PI : sign (M_PI / 2.0, -pole * pt_xy.x);
-    else
-      tatng = atan (-pole * (pt_xy.x / pt_xy.y));
-    theta = M_PI * sign (1.0, pt_xy.x) * (sign (0.5, pole * pt_xy.y) + 0.5) + tatng;
-  
-    return rtheta_t (r, theta);
-  }
-  latlon_t xy_to_latlon (const xy_t & pt_xy)
-  {
-    return rtheta_to_latlon (xy_to_rtheta (pt_xy));
-  }
-  rtheta_t latlon_to_rtheta (const latlon_t & pt_coord)
-  {
-    return rtheta_t 
-  	  (r_equateur * pow (tan ((M_PI / 4.0) - ((pole * pt_coord.lat) / 2.0)), kl),
-             kl * dist_2ref (pt_coord, ref_pt));
-  }
-  xy_t rtheta_to_xy (const rtheta_t & pt_rtheta)
-  {
-    return xy_t (pt_rtheta.r * sin (pt_rtheta.theta), -pole * pt_rtheta.r * cos (pt_rtheta.theta));
-  }
-  xy_t latlon_to_xy (const latlon_t & pt_coord)
-  {
-    return rtheta_to_xy (latlon_to_rtheta (pt_coord));
-  }
-
-};
+const double glgrib_geometry_lambert::rad2deg = 180.0 / M_PI;
+const double glgrib_geometry_lambert::deg2rad = M_PI / 180.0;
+const double glgrib_geometry_lambert::a = 6371229.0;
 
 void glgrib_geometry_lambert::gencoords (float * px, float * py) const
 {
@@ -160,8 +66,8 @@ void glgrib_geometry_lambert::init (codes_handle * h, const float orography)
   xyz = (float *)malloc (3 * sizeof (float) * Nx * Ny);
   numberOfPoints  = Nx * Ny;
 
-  proj_t p_pj (deg2rad * LoVInDegrees, deg2rad * LaDInDegrees, projectionCentreFlag == 128 ? -1.0 : +1.0);
-  xy_t center_xy = p_pj.latlon_to_xy (p_pj.ref_pt);
+  p_pj = proj_t (deg2rad * LoVInDegrees, deg2rad * LaDInDegrees, projectionCentreFlag == 128 ? -1.0 : +1.0);
+  center_xy = p_pj.latlon_to_xy (p_pj.ref_pt);
 
   // Generation of coordinates
 #pragma omp parallel for
@@ -191,11 +97,7 @@ glgrib_geometry_lambert::~glgrib_geometry_lambert ()
 
 int glgrib_geometry_lambert::latlon2index (float lat, float lon) const
 {
-  proj_t p_pj (deg2rad * LoVInDegrees, deg2rad * LaDInDegrees, projectionCentreFlag == 128 ? -1.0 : +1.0); // Costly
-
   xy_t xy = p_pj.latlon_to_xy (latlon_t (lon * deg2rad, lat * deg2rad));
-  xy_t center_xy = p_pj.latlon_to_xy (p_pj.ref_pt);
-
   xy = xy - center_xy;
 
   int i = xy.x / DxInMetres + Nux / 2;
--- glgrib_geometry_lambert.h
+++ glgrib_geometry_lambert.h
@@ -10,6 +10,107 @@
 class glgrib_geometry_lambert : public glgrib_geometry
 {
 public:
+  static const double rad2deg;
+  static const double deg2rad;
+  static const double a;
+  
+  class latlon_t
+  {
+  public:
+    latlon_t () {}
+    latlon_t (double _lon, double _lat) : lon (_lon), lat (_lat) {}
+    double lat = 0.0, lon = 0.0;
+  };
+  
+  class xy_t 
+  {
+  public:
+    xy_t () {}
+    xy_t (double _x, double _y) : x (_x), y (_y) {}
+    double x = 0.0, y = 0.0;
+    xy_t operator+ (const xy_t & p)
+    {
+      return xy_t (x + p.x, y + p.y);
+    }
+    xy_t operator- (const xy_t & p)
+    {
+      return xy_t (x - p.x, y - p.y);
+    }
+  };
+  
+  class rtheta_t
+  {
+  public:
+    rtheta_t (double _r, double _theta) : r (_r), theta (_theta) {}
+    double r, theta;
+  };
+  
+  static inline double sign (const double & a, const double & b)
+  {
+    return b > 0 ? +fabs (a) : -fabs (a);
+  }
+  
+  static inline double dist_2ref (const latlon_t & pt_coord, const latlon_t & ref_coord)
+  {
+    double z = pt_coord.lon - ref_coord.lon;
+    z = z - sign (M_PI, z) * (1.0 + sign (1.0, fabs (z) - M_PI));
+    return -z * sign (1.0, z - M_PI);
+  }
+  
+  class proj_t
+  {
+  public:
+    proj_t () {}
+    proj_t (double lon, double lat, double _pole) : ref_pt (lon, lat), pole (_pole)
+    {
+      kl = pole * sin (ref_pt.lat);
+      r_equateur = a * pow (cos (ref_pt.lat), 1.0 - kl) * pow (1.0 + kl, kl) / kl;
+    }
+    latlon_t ref_pt;
+    double pole;
+    double r_equateur;
+    double kl;
+  
+    latlon_t rtheta_to_latlon (const rtheta_t & pt_rtheta) const 
+    {
+      return latlon_t (ref_pt.lon + pt_rtheta.theta / kl,
+                       pole * ((M_PI / 2.0) - 2.0 * atan(pow (pt_rtheta.r / r_equateur, 1.0 / kl))));
+    }
+    rtheta_t xy_to_rtheta (const xy_t & pt_xy) const 
+    {
+      double r = sqrt(pt_xy.x * pt_xy.x + pt_xy.y * pt_xy.y);
+      double tatng;
+      double theta;
+    
+      if (pt_xy.y == 0.0) 
+        tatng = (pt_xy.x == 0.0) ? M_PI : sign (M_PI / 2.0, -pole * pt_xy.x);
+      else
+        tatng = atan (-pole * (pt_xy.x / pt_xy.y));
+      theta = M_PI * sign (1.0, pt_xy.x) * (sign (0.5, pole * pt_xy.y) + 0.5) + tatng;
+    
+      return rtheta_t (r, theta);
+    }
+    latlon_t xy_to_latlon (const xy_t & pt_xy) const 
+    {
+      return rtheta_to_latlon (xy_to_rtheta (pt_xy));
+    }
+    rtheta_t latlon_to_rtheta (const latlon_t & pt_coord) const 
+    {
+      return rtheta_t 
+    	  (r_equateur * pow (tan ((M_PI / 4.0) - ((pole * pt_coord.lat) / 2.0)), kl),
+               kl * dist_2ref (pt_coord, ref_pt));
+    }
+    xy_t rtheta_to_xy (const rtheta_t & pt_rtheta) const 
+    {
+      return xy_t (pt_rtheta.r * sin (pt_rtheta.theta), -pole * pt_rtheta.r * cos (pt_rtheta.theta));
+    }
+    xy_t latlon_to_xy (const latlon_t & pt_coord) const 
+    {
+      return rtheta_to_xy (latlon_to_rtheta (pt_coord));
+    }
+  
+  };
+
   virtual bool isEqual (const glgrib_geometry &);
   virtual std::string md5 () const;
   virtual int latlon2index (float, float) const;
@@ -22,6 +123,8 @@ public:
 private:
   long int Nx, Ny, Nux, Nuy, projectionCentreFlag;
   double LaDInDegrees, LoVInDegrees, DxInMetres, DyInMetres;
+  proj_t p_pj;
+  xy_t center_xy;
 };
 
 #endif

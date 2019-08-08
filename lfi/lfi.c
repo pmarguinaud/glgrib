59fd9e57132294b0f0054a86fbfa2ed2084f9f92
Philippe Marguinaud <pmarguinaud@hotmail.com>
2019-08-08 23:08:31 +0200

    Store LFI pure data in table

--- lfi/lfi.c
+++ lfi/lfi.c
@@ -101,7 +101,7 @@ typedef struct write_hdr_t
 {
   FILE * fp;
   integer64 dat[22];
-  int off;
+  size_t off;
 } write_hdr_t;
 
 
@@ -109,15 +109,23 @@ static size_t write_hdr (char * ptr, size_t size, size_t nmemb, void * userdata)
 {
   write_hdr_t * hdr = userdata;
   fwrite (ptr, size, nmemb, hdr->fp);
-  memcpy (&hdr->dat[hdr->off], ptr, size * nmemb);
+  memcpy ((char *)hdr->dat + hdr->off, ptr, size * nmemb);
   hdr->off += size * nmemb;
   return size * nmemb;
 }
 
-static size_t write_idx (char * ptr, size_t size, size_t nmemb, void * userdata)
+typedef struct write_dat_t
 {
-  FILE * fp = userdata;
-  fwrite (ptr, size, nmemb, fp);
+  char * ptr;
+  size_t off;
+} write_dat_t;
+
+
+static size_t write_dat (char * ptr, size_t size, size_t nmemb, void * userdata)
+{
+  write_dat_t * dat = userdata;
+  memcpy ((char *)dat->ptr + dat->off, ptr, size * nmemb);
+  dat->off += size * nmemb;
   return size * nmemb;
 }
 
@@ -140,16 +148,27 @@ int feed_LFI_PURE (const char * url, const char * file, sqlite3 * db,
   
   // Get full index
   char range[32];
+  FILE * fp = fopen ("header", "w");
+  if (fp == NULL)
+    goto end;
+
   sprintf (range, "0-%lld", 3*8*hdr->dat[0]-1);
+
+  curl_easy_setopt (hnd, CURLOPT_URL, url);
   curl_easy_setopt (hnd, CURLOPT_RANGE, range);
-  curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, write_idx);
+  curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, fwrite);
+  curl_easy_setopt (hnd, CURLOPT_WRITEDATA, fp);
   
   // We should look at the header to see whether file has extra indexes, and get them too
   
-  FILE * fp = fopen ("header", "w");
-  curl_easy_setopt (hnd, CURLOPT_WRITEDATA, fp);
   ret = curl_easy_perform (hnd);
+
   fclose (fp);
+  
+  curl_easy_setopt (hnd, CURLOPT_URL, NULL);
+  curl_easy_setopt (hnd, CURLOPT_RANGE, NULL);
+  curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, NULL);
+  curl_easy_setopt (hnd, CURLOPT_WRITEDATA, NULL);
   
   if (ret != 0)
     goto end;
@@ -202,7 +221,7 @@ int feed_LFI_PURE (const char * url, const char * file, sqlite3 * db,
     ins = NULL;
   
   
-    TRY (sqlite3_prepare_v2 (db, "INSERT INTO LFI_PURE_INDEX VALUES (?, ?, ?, ?);", -1, &ins, 0));
+    TRY (sqlite3_prepare_v2 (db, "INSERT INTO LFI_PURE_INDEX (IRANK, CLNOMA, IPOSEX, ILONGD) VALUES (?, ?, ?, ?);", -1, &ins, 0));
   
     for (int i = 0; i < INALDO; i++)
       {
@@ -212,8 +231,8 @@ int feed_LFI_PURE (const char * url, const char * file, sqlite3 * db,
         CLNOMA[CLNOMA_len] = '\0';
         TRY (sqlite3_bind_int  (ins, 1, IRANK));
         TRY (sqlite3_bind_text (ins, 2, CLNOMA, strlen (CLNOMA), NULL));
-        TRY (sqlite3_bind_int  (ins, 3, IPOSEX));
-        TRY (sqlite3_bind_int  (ins, 4, ILONGD));
+        TRY (sqlite3_bind_int64  (ins, 3, IPOSEX));
+        TRY (sqlite3_bind_int64  (ins, 4, ILONGD));
         if ((rc = sqlite3_step (ins)) != SQLITE_DONE)
           goto end;
         sqlite3_reset (ins);
@@ -309,6 +328,21 @@ end:
   return IRANK;
 }
 
+CURL * get_curl (CURL * hnd)
+{
+  if (hnd != NULL)
+    return hnd;
+  // Create CURL handle
+  hnd = curl_easy_init();
+  curl_easy_setopt (hnd, CURLOPT_BUFFERSIZE, 102400L);
+  curl_easy_setopt (hnd, CURLOPT_NETRC, (long)CURL_NETRC_REQUIRED);
+  curl_easy_setopt (hnd, CURLOPT_USERAGENT, "curl/7.58.0");
+  curl_easy_setopt (hnd, CURLOPT_MAXREDIRS, 50L);
+  curl_easy_setopt (hnd, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2TLS);
+  curl_easy_setopt (hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+  return hnd;
+}
+
 int test_curl (const char * url, const char * name)
 {
   CURLcode ret;
@@ -318,7 +352,7 @@ int test_curl (const char * url, const char * name)
 
   TRY (sqlite3_open ("lfi.db", &db));
 
-  sqlite3_stmt * del = NULL, * sel = NULL, * ins = NULL;
+  sqlite3_stmt * del = NULL, * sel = NULL, * ins = NULL, * upd = NULL;
 
   int IRANK = -1;
 
@@ -332,14 +366,7 @@ int test_curl (const char * url, const char * name)
   if ((IRANK == -1) && (ft > 0)) // File is missing from database but exists on remote server
     {
       // Create CURL handle
-      hnd = curl_easy_init();
-      curl_easy_setopt (hnd, CURLOPT_BUFFERSIZE, 102400L);
-      curl_easy_setopt (hnd, CURLOPT_URL, url);
-      curl_easy_setopt (hnd, CURLOPT_NETRC, (long)CURL_NETRC_REQUIRED);
-      curl_easy_setopt (hnd, CURLOPT_USERAGENT, "curl/7.58.0");
-      curl_easy_setopt (hnd, CURLOPT_MAXREDIRS, 50L);
-      curl_easy_setopt (hnd, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2TLS);
-      curl_easy_setopt (hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+      hnd = get_curl (hnd);
      
       // Get first 22*8 characters of remote file
       
@@ -347,7 +374,11 @@ int test_curl (const char * url, const char * name)
       write_hdr_t hdr;
       memset (&hdr, 0, sizeof (hdr));
       hdr.fp = fopen (file, "w");
+
+      if (hdr.fp == NULL) 
+        goto end;
      
+      curl_easy_setopt (hnd, CURLOPT_URL, url);
       curl_easy_setopt (hnd, CURLOPT_RANGE, "0-175");
       curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, write_hdr);
       curl_easy_setopt (hnd, CURLOPT_WRITEDATA, &hdr);
@@ -355,6 +386,11 @@ int test_curl (const char * url, const char * name)
       ret = curl_easy_perform (hnd);
      
       fclose (hdr.fp);
+     
+      curl_easy_setopt (hnd, CURLOPT_URL, NULL);
+      curl_easy_setopt (hnd, CURLOPT_RANGE, NULL);
+      curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, NULL);
+      curl_easy_setopt (hnd, CURLOPT_WRITEDATA, NULL);
      
       if (ret != 0)
         goto end;
@@ -380,7 +416,7 @@ int test_curl (const char * url, const char * name)
   
   {
     char CLNOMA[17];
-    TRY (sqlite3_prepare_v2 (db, "SELECT IPOSEX, ILONGD FROM LFI_PURE_INDEX "
+    TRY (sqlite3_prepare_v2 (db, "SELECT IPOSEX, ILONGD, DATA FROM LFI_PURE_INDEX "
                              "WHERE IRANK = ? AND CLNOMA = ?;", -1, &sel, 0));
     TRY (sqlite3_bind_int (sel, 1, IRANK));
 
@@ -392,11 +428,73 @@ int test_curl (const char * url, const char * name)
     TRY (sqlite3_bind_text (sel, 2, CLNOMA, strlen (CLNOMA), NULL));
 
     rc = sqlite3_step (sel);
+
     if (rc == SQLITE_ROW)
       {
-        int IPOSEX = sqlite3_column_int (sel, 0);
-        int ILONGD = sqlite3_column_int (sel, 1);
-        printf (" IPOSEX = %d, ILONGD = %d\n", IPOSEX, ILONGD);
+        rc = SQLITE_OK;
+
+        ssize_t IPOSEX = sqlite3_column_int64 (sel, 0);
+        ssize_t ILONGD = sqlite3_column_int64 (sel, 1);
+        const void * DATA = sqlite3_column_blob (sel, 2);
+        int DATA_size = sqlite3_column_bytes (sel, 2);
+
+        printf (" IPOSEX = %ld, ILONGD = %ld, DATA_size = %d\n", IPOSEX, ILONGD, DATA_size);
+
+        if (DATA_size == 0)
+          {
+            size_t offset = (IPOSEX-1) * 8, length = ILONGD * 8;
+            char range[64];
+            write_dat_t dat;
+            memset (&dat, 0, sizeof (dat));
+            dat.ptr = (char *)malloc (length);
+
+            sprintf (range, "%ld-%ld", offset, offset + length - 1);
+
+            hnd = get_curl (NULL);
+
+            curl_easy_setopt (hnd, CURLOPT_URL, url);
+            curl_easy_setopt (hnd, CURLOPT_RANGE, range);
+            curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, write_dat);
+            curl_easy_setopt (hnd, CURLOPT_WRITEDATA, &dat);
+
+            
+            ret = curl_easy_perform (hnd);
+
+            printf (" ret = %d\n", ret);
+
+            curl_easy_setopt (hnd, CURLOPT_URL, NULL);
+            curl_easy_setopt (hnd, CURLOPT_RANGE, NULL);
+            curl_easy_setopt (hnd, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt (hnd, CURLOPT_WRITEDATA, NULL);
+             
+
+            if (ret != 0)
+              goto end;
+
+            {
+              int t = 8, n = ILONGD;
+              iswap_ (dat.ptr, dat.ptr, &t, &n);
+            }
+
+            TRY (sqlite3_prepare_v2 (db, "UPDATE LFI_PURE_INDEX SET DATA = ? "
+                                     "WHERE IRANK = ? AND CLNOMA = ?;", -1, &upd, 0));
+
+            TRY (sqlite3_bind_blob64 (upd, 1, dat.ptr, length, NULL));
+            TRY (sqlite3_bind_int (upd, 2, IRANK));
+            TRY (sqlite3_bind_text (upd, 3, CLNOMA, strlen (CLNOMA), NULL));
+
+            if ((rc = sqlite3_step (upd)) != SQLITE_DONE)
+              goto end;
+
+            TRY (sqlite3_finalize (upd));
+            upd = NULL;
+
+            free (dat.ptr);
+
+          }
+
+
+
       }
     else if (rc = SQLITE_DONE)
       {
@@ -412,6 +510,8 @@ end:
 
   if (rc != SQLITE_OK)
     printf ("%s\n", sqlite3_errmsg (db));
+  if (upd != NULL)
+    sqlite3_finalize (upd);
   if (ins != NULL)
     sqlite3_finalize (ins);
   if (del != NULL)

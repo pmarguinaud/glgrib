98170408ad892947f557dbbc1f40b2169754e885
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2019-12-03 13:31:35 +0100

    Fix expand.pl script

--- shaders/expand.pl
+++ shaders/expand.pl
@@ -8,6 +8,7 @@ use File::Basename;
 
 sub load
 {
+  my $f = shift;
   my @text = do { my $fh = 'FileHandle'->new ("<$f"); <$fh> };
   return @text;
 }
@@ -21,12 +22,13 @@ for my $f (<shaders/src/*.vs>, <shaders/src/*.fs>)
         if ($text =~ m/^#include\s+"([^"]+)"\s*$/o)
           {
             my $h = $1;
-            push @code, "// start include $h\n", &load ($h), "// end include $h\n";
+            push @code, "// start include $h\n", &load ("shaders/include/$h"), "// end include $h\n";
 	  }
 	else
 	  {
             push @code, $text;
 	  }
       }
+    $f = &basename ($f);
     'FileHandle'->new (">shaders/$f")->print (join ('', @code));
   }

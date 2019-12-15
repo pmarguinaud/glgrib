f2ad2d9b9c80b4caba2264ac01d9f7cb88497e65
Philippe Marguinaud <pmarguinaud@hotmail.com>
2019-12-15 19:26:36 +0100

    Script for derivatives

---
+++ derivgp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+id=18215
+
+
+./glgrib.x \
+--field[0].path ftp://prolix.meteo.fr//scratch/work/marguina/tmp/tmp.$id/MYPROC.001.fa%TESTFIELD \
+   --field[0].palette-{ --colors blue red green yellow pink white red green blue  --values 0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 9.5 }-  \
+   --view.lat 67.49 --view.lon 26.92 --view.fov 0.3 --field[0].scalar.points.on --window.offscreen.format myproc.png --window.offscreen.on
+
+for d in 1 2 3
+do
+for k in "" 1
+do
+
+./glgrib.x --view.lat 67.49 --view.lon 26.92 --view.fov 0.3 \
+    --field[0].path ftp://prolix.meteo.fr//scratch/work/marguina/tmp/tmp.$id/ZFLDDY$k.00$d.fa%TESTFIELD --field[0].type contour  --field[0].palette.colors blue --field[0].contour-{ --min -0.25 --max 0.25 }- \
+    --field[1].path ftp://prolix.meteo.fr//scratch/work/marguina/tmp/tmp.$id/ZFLDDX$k.00$d.fa%TESTFIELD --field[1].type contour  --field[1].palette.colors red  --field[1].contour-{ --min -0.25 --max 0.25 }- \
+    --field[2].path ftp://prolix.meteo.fr//scratch/work/marguina/tmp/tmp.$id/ZFLD.00$d.fa%TESTFIELD --field[2].palette.name Greens --field[2].scale 0.999 --field[2].scalar.wireframe.on \
+    --window.offscreen.on --window.offscreen.format gpderiv$k.$d.png
+
+done
+done

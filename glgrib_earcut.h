dc81cdd5b1794ad80c07fd224d4ac0b3115f1fc4
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2020-01-13 09:51:32 +0100

    Move ear cutting to independant file

---
+++ glgrib_earcut.h
@@ -0,0 +1,13 @@
+#ifndef _GLGRIB_EARCUT_H
+#define _GLGRIB_EARCUT_H
+
+#include <vector>
+
+namespace glgrib_earcut
+{
+void processRing (const std::vector<float> &, 
+                  int, int, int, int,
+                  std::vector<unsigned int> *, bool);
+}
+
+#endif

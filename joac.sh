680e47c444dbad65cafae0f30a240bd539f8cc0a
Philippe Marguinaud <pmarguinaud@hotmail.com>
2020-01-31 09:23:16 +0100

    Modify test script

--- joac.sh
+++ joac.sh
@@ -10,7 +10,7 @@ set -x
   --field[0].palette.linear.on --colorbar.on \
   --field[1].path '/home/phi001/tmp/joachim_surf.grib%stepRange=24,shortName="10u"' \
                   '/home/phi001/tmp/joachim_surf.grib%stepRange=24,shortName="10v"' \
-  --field[1].scale 1.001 --field[1].type vector --field[1].vector.hide_norm.on --field[1].vector.color black \
+  --field[1].scale 1.001 --field[1].type vector --field[1].vector.norm.off --field[1].vector.arrow.color black \
   --field[1].vector.density 20 \
   --field[2].path '/home/phi001/tmp/joachim_surf.grib%stepRange=24,shortName="msl"' \
   --field[2].scale 1.001 --field[2].type contour --field[2].palette.colors black \
@@ -18,5 +18,9 @@ set -x
   --field[2].contour.widths 3 3 3 3 1 1 1 1 1 1 3 \
   --window.width 1200 --grid.on --grid.color black --grid.resolution 18 \
   --coast.on --coast.lines.color green \
-  --landscape.on --landscape.path landscape/white.png --view.clip.xmin 0.15 --view.projection POLAR_NORTH \
+  --view.clip.xmin 0.15 --view.projection POLAR_NORTH \
+  --land.on --land.layers[0].color '#7c879cff' \
   --view.lon 4 --view.lat 51 --view.fov 3
+
+
+# --landscape.on --landscape.path landscape/white.png \

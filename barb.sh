ae12b5d9a3bb75ad4082f1479a35725f9d54904a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2020-02-17 13:51:32 +0100

    Cleaning

--- barb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-make
-
-
-./glgrib.x --field[0].path testdata/aro2.5/S090WIND.U.PHYS.grb testdata/aro2.5/S090WIND.V.PHYS.grb --field[0].type VECTOR  --view-{ --lon 2 --lat 46.2 --fov 3 }- --field[0].vector.barb.color red --field[0].vector.arrow.off --coast.lines.color cyan --coast.on --field[0].vector.norm.off --field[0].vector.scale 1   --field[0].vector.barb.on

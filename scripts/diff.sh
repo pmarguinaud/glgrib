b106827e2b0348371b106fa74ad0026c6464a5c8
Philippe Marguinaud <pmarguinaud@hotmail.com>
2020-04-14 14:40:41 +0200

    Fix diff.sh script

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./glGrib.x --diff.on --diff.path testdata/diff/ICMSHFCST+0001 testdata/diff/ICMSHFCST+0002 --scene.title.on  --colorbar.on --window.width 900
+./bin/glgrib --diff.on --diff.path share/data/diff/ICMSHFCST+0001 share/data/diff/ICMSHFCST+0002 --scene.title.on  --colorbar.on --window.width 900

df364d586fec83765671f341b24bc108967a30c4
floppy31 <floppy@s-entraider.net>
2020-04-24 14:07:53 +0200

    helper v0

--- src/python/gl_plot.py
+++ src/python/gl_plot.py
@@ -6,6 +6,7 @@ import six
 import argparse
 import os 
 import glGrib
+import json
 
 
 def is_valid_file(parser, arg):
@@ -19,6 +20,9 @@ def is_valid_file(parser, arg):
             parser.error("The file %s is not a grib nor an FA" % arg)
         return arg  
 
+def click():
+    print('toto')
+    
 def main(file_path=None,cmd=None,info=None,all=None,field=None):
 
     if cmd:
@@ -27,6 +31,7 @@ def main(file_path=None,cmd=None,info=None,all=None,field=None):
         except:
             raise argparse.ArgumentTypeError('--cmd argument not valid: {}'.format(cmd))
         glGrib.start(*command)
+
         glGrib.sleep(200)
     elif info:
         if field:
@@ -41,6 +46,30 @@ def main(file_path=None,cmd=None,info=None,all=None,field=None):
     else:
         print("DBG :",file_path)
 
+
+
+
+
+
+def configureCanvas(event):
+    canvas.configure(scrollregion=canvas.bbox("all"),width=1700,height=500)
+
+def data(listOpts):
+    for item in listOpts:
+        opt=item[0]
+        type=item[1]
+        name=item[2]
+        default=""
+        if len(item)>3:
+            default=item[3]
+        i=listOpts.index(item)    
+        Label(frame,text=name).grid(row=i,column=0)
+        Label(frame,text=type).grid(row=i,column=1)
+        Label(frame,text=default).grid(row=i,column=2)
+       
+
+        
+
 helpCmd="""Mimic the raw glgrib binary command line behaviour.\n `gl_plot.py --cmd "glgrib_options"`
 is equivalent to \n
 `glbrib glgrib_options` (the `"` are needed around glgrib_options)"""
@@ -54,6 +83,11 @@ helpA="""Plot all fields of `file_path`
 helpF="""Plot only the field `field_name`"""
 
 helpPath="""This is the main argument of the program. Must be None with the -c/--cmd option"""
+
+
+
+
+
 
 if __name__ == "__main__":
     # 1. Parse arguments
@@ -77,6 +111,55 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
+    
+    glGrib.start ()
+    f=open('glgrib.json','w')
+    f.write(glGrib.json ())
+    f.close()
+    listOpts=json.loads(glGrib.json ())
+    
+    from Tkinter import * 
+
+    root=Tk()
+    sizex = 1800
+    sizey = 600
+    posx  = 100
+    posy  = 100
+    root.wm_geometry("%dx%d+%d+%d" % (sizex, sizey, posx, posy))
+    
+    myframe=Frame(root,relief=GROOVE,width=1700,height=500,bd=1)
+    myframe.place(x=10,y=10)
+    
+    canvas=Canvas(myframe)
+    frame=Frame(canvas)
+    myscrollbar=Scrollbar(myframe,orient="vertical",command=canvas.yview)
+    canvas.configure(yscrollcommand=myscrollbar.set)
+    
+    myscrollbar.pack(side="right",fill="y")
+    canvas.pack(side="left")
+    canvas.create_window((0,0),window=frame,anchor='nw')
+    frame.bind("<Configure>",configureCanvas)
+    data(listOpts)
+    root.mainloop()
+
+
+
+
+    fenetre = Tk()
+    for item in listOpts:
+        print(item)
+        opt=item[0]
+        type=item[1]
+        name=item[2]
+        default=""
+        if len(item)>3:
+            default=item[3]
+        
+        label = Label(fenetre, text=name)
+        label.pack()
+    
+    
+    fenetre.mainloop()    
     
     main(**vars(args))
     

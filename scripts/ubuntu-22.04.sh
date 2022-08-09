c57b578082e97dc06c388f871859d8097ece656f
Philippe Marguinaud <pmarguinaud@hotmail.com>
2022-08-09 20:17:04 +0200

    Create deb package

---
+++ scripts/ubuntu-22.04.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -x
+
+dir=glgrib_1.0-1_amd64
+mkdir -p $dir
+
+
+function ss ()
+{
+  src=$1
+  dst=$2
+  mkdir -p $dir/$dst
+  cp -alf $src/* $dir/$dst
+}
+
+rm lib/libLFI.a
+chrpath -d bin/*
+
+ss bin usr/bin
+ss lib lib/x86_64-linux-gnu
+ss share/glgrib usr/share/glgrib
+
+export DEBEMAIL="pmarguinaud@hotmail.com"
+export DEBFULLNAME="Philippe Marguinaud"
+
+cd $dir
+mkdir -p debian
+touch debian/control
+
+cat -> debian/control << EOF
+Package: glgrib
+Version: 1.0
+Architecture: amd64
+Maintainer: $DEBFULLNAME <$DEBEMAIL>
+Provides: glgrib
+Source: glgrib
+Description: Display GRIB data with OpenGL.
+ More info at https://github.com/pmarguinaud/glgrib.
+EOF
+
+dpkg-shlibdeps -O usr/bin/glgrib_batch  | perl -pe 's/shlibs:Depends=/Depends: /o;'  > depends.txt
+
+cat depends.txt >> debian/control
+\rm depends.txt
+
+mv debian DEBIAN
+
+cd ..
+
+dpkg-deb --build --root-owner-group glgrib_1.0-1_amd64 

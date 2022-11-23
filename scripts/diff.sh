c2d3fe9f5818f5fd5f87762978e5d72350360a53
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-11-23 12:31:46 +0100

    Update diff.sh script

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/glgrib --diff.on --diff.path share/data/diff/ICMSHFCST+0001 share/data/diff/ICMSHFCST+0002 --scene.title.on  --colorbar.on --window.width 900
+./bin/glgrib --diff.on --diff.path share/glgrib/data/diff/ICMSHFCST+0001 share/glgrib/data/diff/ICMSHFCST+0002 --scene.title.on  --colorbar.on --render.width 900

ad1a3694ace760f385912d4fd62dba227156847b
Philippe Marguinaud <pmarguinaud@hotmail.com>
2022-12-12 15:11:00 +0100

    Use shared directory (docker --mount=...)

--- scripts/debian-setup.sh
+++ scripts/debian-setup.sh
@@ -15,13 +15,95 @@ DEBIAN=latest
 VERSION=1.0
 PACKVER=1
 
-if [ 1 -eq 1 ]
-then
 sudo docker pull debian:$DEBIAN
 
 SSHKEY=$(cat $HOME/.ssh/id_rsa.pub)
 
-cat > boot.sh << EOF
+SHARED=$PWD/debuild
+
+\rm -rf $SHARED
+mkdir -p $SHARED
+
+mkdir -p $SHARED/.ssh
+cat > $SHARED/.ssh/authorized_keys << EOF
+$SSHKEY
+EOF
+
+chmod 600 $SHARED/.ssh/authorized_keys
+
+cat -> $SHARED/.bash_profile << EOF
+
+export EMAIL=$EMAIL
+unset LANG
+
+set -o vi
+
+EOF
+
+cat -> $SHARED/.vimrc << EOF
+:set mouse=
+:set hlsearch
+EOF
+
+cat > $SHARED/glgrib-build.sh << EOF
+#!/bin/bash
+
+set -x
+set -e
+
+git config --global user.email "$EMAIL"
+git config --global user.name "$NAME"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+
+git clone -b debian https://github.com/pmarguinaud/glgrib.git
+cd glgrib
+./scripts/debian.pl --create-archive --create-directory 
+./scripts/debian.pl --compile
+EOF
+ 
+cat > $SHARED/glgrib-install.sh << EOF
+#!/bin/bash
+
+set -x
+set -e
+
+for pack in data doc test bin
+do
+  set +e
+  sudo dpkg -i ./glgrib-\${pack}_${VERSION}-${PACKVER}_amd64.deb  
+  set -e
+done
+
+sudo apt-get install -y -f
+
+EOF
+
+cat > $SHARED/glgrib-uninstall.sh << EOF
+#!/bin/bash
+
+set -x
+set -e
+
+for pack in bin data doc test
+do
+  sudo apt-get remove -y glgrib-\$pack
+done
+sudo apt-get autoremove -y 
+
+EOF
+
+cat > $SHARED/glgrib-test.sh << EOF
+#!/bin/bash
+
+set -x
+set -e
+
+glgrib --grid.on
+
+EOF
+
+cat > $SHARED/glgrib-boot.sh << EOF
 #!/bin/bash
 
 set -x
@@ -52,131 +134,27 @@ service ssh start
 
 useradd $USER
 chsh -s /bin/bash $USER
-mkdir -p /home/$USER/.ssh
-chown -R $USER:users /home/$USER
-
-cat > /home/$USER/.ssh/authorized_keys << EOC
-$SSHKEY
-EOC
-
-chmod 600 /home/$USER/.ssh/authorized_keys
-
-sudo -u $USER ssh-keygen -t rsa -q -f "/home/$USER/.ssh/id_rsa" -N ""
-
-cat -> /home/$USER/.bash_profile << EOC
-
-export EMAIL=$EMAIL
-unset LANG
-
-set -o vi
-
-EOC
-
-cat -> /home/$USER/.vimrc << EOC
-:set mouse=
-:set hlsearch
-EOC
-
-chown $USER:users /home/$USER/.ssh/authorized_keys /home/$USER/.bash_profile /home/$USER/.vimrc
 
 cat >> /etc/sudoers << EOC
 
 $USER ALL=(root) NOPASSWD: /usr/bin/dpkg, NOPASSWD: /usr/bin/apt, NOPASSWD: /usr/bin/apt-get, NOPASSWD: /usr/bin/dpkg
 EOC
 
-if [ "x\$kind" = "xbuild" ]
-then
-  cd /home/$USER
-  cat > glgrib-build.sh << EOC
-#!/bin/bash
-
-set -x
-set -e
-
-git config --global user.email "$EMAIL"
-git config --global user.name "$NAME"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
-
-git clone -b debian https://github.com/pmarguinaud/glgrib.git
-cd glgrib
-./scripts/debian.pl --create-archive --create-directory 
-./scripts/debian.pl --compile
-EOC
- 
-  chown $USER:users glgrib-*.sh
-  chmod +x glgrib-*.sh
-
-fi
-
-if [ "x\$kind" = "xinstall" ]
-then
-  cd /home/$USER
-  cat > glgrib-install.sh << EOC
-#!/bin/bash
-
-set -x
-set -e
-
-for pack in data doc test bin
-do
-  set +e
-  sudo dpkg -i ./glgrib-\\\${pack}_${VERSION}-${PACKVER}_amd64.deb  
-  set -e
-done
-
-sudo apt-get install -f
-
-EOC
-
-  cat > glgrib-uninstall.sh << EOC
-#!/bin/bash
-
-set -x
-set -e
-
-for pack in bin data doc test
-do
-  sudo apt-get remove -y glgrib-\\$pack
-done
-sudo apt-get autoremove -y 
-
-EOC
-
-  cat > glgrib-test.sh << EOC
-#!/bin/bash
-
-set -x
-set -e
-
-glgrib --grid.on
-
-EOC
-
-  chown $USER:users glgrib-*.sh
-  chmod +x glgrib-*.sh
-
-fi
-
 EOF
 
-chmod +x boot.sh
+chmod +x $SHARED/*.sh
 
 declare -A IP
-
-mkdir -p debuild
 
 for kind in build install
 do
   sudo docker \
     run -h debian_glgrib_$kind -t -d \
     --name debian_glgrib_$kind \
-    --mount type=bind,src=$PWD/debuild/,dst=/home/$USER/debuild \
+    --mount type=bind,src=$SHARED/,dst=/home/$USER \
     debian:$DEBIAN
   
-  sudo docker cp boot.sh debian_glgrib_$kind:/root/boot.sh
-  
-  sudo docker exec debian_glgrib_$kind /root/boot.sh $kind
+  sudo docker exec debian_glgrib_$kind /home/$USER/glgrib-boot.sh $kind
   
   IP[$kind]=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" debian_glgrib_$kind)
   ssh-keygen -f "$HOME/.ssh/known_hosts" -R ${IP[$kind]}
@@ -184,37 +162,14 @@ do
   echo "${IP[$kind]}" > debian_glgrib_$kind.txt
 done
 
-fi
 
 SSH="ssh -o StrictHostKeyChecking=no -X"
-SCP="scp -o StrictHostKeyChecking=no"
-
-
-declare -A IP
-
-for kind in build install
-do
-  IP[$kind]=$(sudo docker inspect -f "{{ .NetworkSettings.IPAddress }}" debian_glgrib_$kind)
-done
-
-
-if [ 1 -eq 1 ]
-then
 
 # Build
 
 $SSH ${IP[build]} ./glgrib-build.sh
 
-fi
-
-# Copy deb & install
-
-for pack in bin doc data test
-do
-  $SCP \
-    ${IP[build]}:glgrib-${pack}_${VERSION}-${PACKVER}_amd64.deb \
-    ${IP[install]}:glgrib-${pack}_${VERSION}-${PACKVER}_amd64.deb
-done
+# Install
 
 $SSH ${IP[install]} ./glgrib-install.sh
 

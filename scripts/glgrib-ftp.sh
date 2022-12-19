2d889d3a91b2d02fa74ed30934290823ad9a677a
Philippe Marguinaud <pmarguinaud@hotmail.com>
2022-12-19 18:40:26 +0100

    Demo script for FTP

---
+++ scripts/glgrib-ftp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./bin/glgrib ftp://marguina%40belenos.meteo.fr@137.129.13.180:8021//home/gmap/mrpm/marguina/pack/46t1_distatlasLATEST.01.I185274INTELMPI185274.x/Const.Clim.m12.pack%SURFAEROS.SEA
+

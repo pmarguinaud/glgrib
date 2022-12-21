1a06c10a58ebabe0cd36f1c6e59a65baf2e10f45
Philippe Marguinaud <pmarguinaud@hotmail.com>
2022-12-21 16:31:36 +0100

    Fix python3/setup.py

--- src/python3/setup.py
+++ src/python3/setup.py
@@ -10,14 +10,17 @@ debian = os.getenv ('DEB_BUILD_ARCH')
 if (sys.argv[1] == 'install') and (debian is not None):
   sys.argv.append ('--prefix=' + prefix + "/debian/tmp/usr")
 
+if (debian is not None):
+  prefix = '/usr'
+
 setup (
     name = "glGrib.glfw",
     version = "0.1",
     ext_modules = [Extension ("glGrib.glfw", ["glfw.cc"], 
     include_dirs=[TOP + '/include'], 
     define_macros=[('GLGRIB_PREFIX', '"' + prefix + '"'),('USE_GLFW','1')],
-    libraries=['glGrib', 'LFI', 'GLEW', 'GL', 'glfw', 'png', 'readline', 'ncurses',
+    libraries=['glGrib-glfw', 'LFI', 'GLEW', 'GL', 'glfw', 'png', 'readline', 'ncurses',
                'tinfo', 'ssl', 'crypto', 'pthread', 'sqlite3', 'curl', 'shp', 'eccodes'],
-    library_dirs=[prefix + "/lib", "../lib"])],
+    library_dirs=[prefix + "/lib", TOP + "/lib"])],
   );
 
